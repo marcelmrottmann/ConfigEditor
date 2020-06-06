532a3ac6ba68303eb40b711b7e09629fd1016c49
marcelmrottmann <66241711+marcelmrottmann@users.noreply.github.com>
2020-06-06 15:19:29 +0100

    Update auto_update_from_GIT.js

--- js/auto_update_from_GIT.js
+++ js/auto_update_from_GIT.js
@@ -81,7 +81,7 @@ function ReadFiles(x) {
     console.log('true')
         ncp("./temp/ConfigEditor-master", "./", function (err) {
             if (err) {
-              
+              window.alert('Failed to download the new version, the programme will try again next time you reload the programme.')
               PackageJSON = JSON.parse(fs.readFileSync('./package.json'))
               PackageJSON.version = "1.0.0"
               fs.writeFileSync('./package.json',JSON.stringify(PackageJSON))
