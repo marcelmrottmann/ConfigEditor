67582a982ee25e3dae2dae74eb9be129a6a6c4d5
marcelmrottmann <66241711+marcelmrottmann@users.noreply.github.com>
2020-07-29 14:15:13 +0100

    Add files via upload

--- js/app_PatternTemplates.js
+++ js/app_PatternTemplates.js
@@ -401,21 +401,39 @@ function RenderHandsOnTable(x) {
 		//}
 	}
 
+	if (document.getElementById('Status').value == 'PT') {
+		headers = [
+			"id",
+			"name",
+			"description",
+			"timePeriodType",
+			"dayCount",
+			"startDate",
+			"anchorStartDay",
+			"day",
+			"shiftTemplates",
+			"availabilities",
+			"paycodes"
+		]
+	}
 
-
-	headers = [
-		"id",
-		"name",
-		"description",
-		"timePeriodType",
-		"dayCount",
-		"startDate",
-		"anchorStartDay",
-		"day",
-		"shiftTemplates",
-		"availabilities",
-		"paycodes"
-	]
+	else {
+		headers = [
+			"personNumber",
+			"StartDate",
+			"EndDate",
+			"name",
+			"description",
+			"timePeriodType",
+			"dayCount",
+			"startDate",
+			"anchorStartDay",
+			"day",
+			"shiftTemplates",
+			"availabilities",
+			"paycodes"
+		]
+	}
 
 
 	FinalArray = Complete
@@ -676,19 +694,42 @@ function downloadNewFile(change_data) {
 
 	ListOfDVs = []
 	for (let i = 0, l = ObjectArray.length; i < l; i++) {
+		if (document.getElementById('Status').value == 'PT') {
+			DetailGenie =
+			{
+				"Name": ObjectArray[i].name,
+				"id": ObjectArray[i].id || "NOID",
+				"Status": "None"
+			}
+			if (i == 0 && ObjectArray[i].name != '' && ObjectArray[i].name != null) {
+				ListOfDVs.push(DetailGenie)
+			}
+			else if (ObjectArray[i - 1].name != ObjectArray[i].name && ObjectArray[i].name != '' && ObjectArray[i].name != null) {
+				ListOfDVs.push(DetailGenie)
+			}
+		}
+		else {
+			DetailGenie =
+			{
+				"Name": ObjectArray[i].personNumber + '--' + ObjectArray[i].name,
+				"id": ObjectArray[i].id || "NOID",
+				"Status": "None"
+			}
+			if (i == 0 && ObjectArray[i].name != '' && ObjectArray[i].name != null && ObjectArray[i].personNumber != '' && ObjectArray[i].personNumber != null) {
+				ListOfDVs.push(DetailGenie)
+			}
+			else if (ObjectArray[i - 1].name != ObjectArray[i].name
+				&& ObjectArray[i - 1].personNumber != ObjectArray[i].personNumber
+				&& ObjectArray[i].name != '' && ObjectArray[i].name != null
+				&& ObjectArray[i].personNumber != '' && ObjectArray[i].personNumber != null) {
+				ListOfDVs.push(DetailGenie)
+			}
 
-		DetailGenie =
-		{
-			"Name": ObjectArray[i].name,
-			"id":ObjectArray[i].id ||"NOID",
-			"Status": "None"
+
+
+
 		}
-		if (i == 0 && ObjectArray[i].name != '' && ObjectArray[i].name != null) {
-			ListOfDVs.push(DetailGenie)
-		}
-		else if (ObjectArray[i - 1].name != ObjectArray[i].name && ObjectArray[i].name != '' && ObjectArray[i].name != null) {
-			ListOfDVs.push(DetailGenie)
-		}
+
 	}
 	ListOfDVs = Array.from(new Set(ListOfDVs))
 	console.log(ListOfDVs)
@@ -702,7 +743,7 @@ function downloadNewFile(change_data) {
 	]
 
 	ColumnHeaders = [
-		"Status", "Name","id"
+		"Status", "Name", "id"
 	]
 	var LoaderHOT = new Handsontable(LoadingHandsOnTable, {
 		data: ListOfDVs,
@@ -734,151 +775,333 @@ function downloadNewFile(change_data) {
 
 	LoadingData = LoaderHOT.getData()
 	console.log(LoadingData)
-
-	LoadedDataArray = []
-	for (let y = 0, z = LoadingData.length; y < z; y++) {
-		LoadedData = ""
-		for (let i = 0, l = ObjectArray.length; i < l; i++) {
-			if (LoadingData[y][1] == ObjectArray[i].name) {
-				LoadedData =
-				{
-					"name": ObjectArray[i].name,
-					"description": ObjectArray[i].description,
-					"timePeriodType": {
-						"name": ObjectArray[i].timePeriodType
-					},
-					"dayCount": ObjectArray[i].dayCount,
-					"startDate": ObjectArray[i].startDate,
-					"items": [],
-					"anchorStartDay": ObjectArray[i].anchorStartDay
+	if (document.getElementById('Status').value == 'PT') {
+		LoadedDataArray = []
+		for (let y = 0, z = LoadingData.length; y < z; y++) {
+			LoadedData = ""
+			for (let i = 0, l = ObjectArray.length; i < l; i++) {
+				if (LoadingData[y][1] == ObjectArray[i].name) {
+					LoadedData =
+					{
+						"name": ObjectArray[i].name,
+						"description": ObjectArray[i].description,
+						"timePeriodType": {
+							"name": ObjectArray[i].timePeriodType
+						},
+						"dayCount": ObjectArray[i].dayCount,
+						"startDate": ObjectArray[i].startDate,
+						"items": [],
+						"anchorStartDay": ObjectArray[i].anchorStartDay
+					}
 				}
 			}
+			LoadedDataArray.push(LoadedData)
 		}
-		LoadedDataArray.push(LoadedData)
-	}
-	for (let y = 0, z = LoadedDataArray.length; y < z; y++) {
-		for (let i = 0, l = ObjectArray.length; i < l; i++) {
-			if (LoadedDataArray[y].name == ObjectArray[i].name) {
-				console.log(ObjectArray[i])
-				if (ObjectArray[i].availabilities != null && ObjectArray[i].availabilities != "") {
-					availTempObject =
-					{
-						"day": ObjectArray[i].day,
-						"availabilityTemplate": {
-							"systemGenerated": true,
-							"segments": []
-						}
-					}
-
-					for (let zi = 0, zl = ObjectArray[i].availabilities.length; zi < zl; zi++) {
-
-						availTemp = ObjectArray[i].availabilities[zi].split('|')
-						availTempObject.availabilityTemplate.segments.push(
+		for (let y = 0, z = LoadedDataArray.length; y < z; y++) {
+			for (let i = 0, l = ObjectArray.length; i < l; i++) {
+				if (LoadedDataArray[y].name == ObjectArray[i].name) {
+					console.log(ObjectArray[i])
+					if (ObjectArray[i].availabilities != null && ObjectArray[i].availabilities != "") {
+						availTempObject =
 						{
-							"availabilityType": {
-								"qualifier": availTemp[0]
-							},
-							"startTime": availTemp[1],
-							"endTime": availTemp[2]
+							"day": ObjectArray[i].day,
+							"availabilityTemplate": {
+								"systemGenerated": true,
+								"segments": []
+							}
 						}
-						)
-						
+
+						for (let zi = 0, zl = ObjectArray[i].availabilities.length; zi < zl; zi++) {
+
+							availTemp = ObjectArray[i].availabilities[zi].split('|')
+							availTempObject.availabilityTemplate.segments.push(
+								{
+									"availabilityType": {
+										"qualifier": availTemp[0]
+									},
+									"startTime": availTemp[1],
+									"endTime": availTemp[2]
+								}
+							)
+
+						}
+						LoadedDataArray[y].items.push(availTempObject)
+
 					}
-					LoadedDataArray[y].items.push(availTempObject)
+					else if (ObjectArray[i].shiftTemplates != null && ObjectArray[i].shiftTemplates != "") {
+
+						STTempObject =
+						{
+							"day": ObjectArray[i].day,
+							"shiftTemplate": {
+								"name": ObjectArray[i].shiftTemplates,
+							}
+						}
+						LoadedDataArray[y].items.push(STTempObject)
+					}
+					else if (ObjectArray[i].paycodes != null && ObjectArray[i].paycodes != "") {
+						availTemp = ObjectArray[i].paycodes.split('|')
+						if (availTemp[1] > availTemp[2]) { EndOffSet = 2 } else { EndOffSet = 1 }
+						durationInTime = moment("1900-01-0" + EndOffSet + "T" + availTemp[2]).diff(("1900-01-01T" + availTemp[1]), 'seconds')
+
+						availTempObject =
+						{
+							"day": ObjectArray[i].day,
+							"payCodeEditTemplate": {
+								"systemGenerated": true,
+								"startDateTime": "1900-01-01T" + availTemp[1],
+								"endDateTime": "1900-01-0" + EndOffSet + "T" + availTemp[2],
+								"payCode": {
+									"qualifier": availTemp[0]
+								},
+								"durationInTime": durationInTime,
+							}
+						}
+						LoadedDataArray[y].items.push(availTempObject)
+					}
 
 				}
-				else if (ObjectArray[i].shiftTemplates != null && ObjectArray[i].shiftTemplates != "") {
-
-					STTempObject =
-					{
-						"day": ObjectArray[i].day,
-						"shiftTemplate": {
-							"name": ObjectArray[i].shiftTemplates,
-						}
-					}
-					LoadedDataArray[y].items.push(STTempObject)
-				}
-				else if (ObjectArray[i].paycodes != null && ObjectArray[i].paycodes != "") {
-					availTemp = ObjectArray[i].paycodes.split('|')
-					if (availTemp[1] > availTemp[2]) { EndOffSet = 2 } else { EndOffSet = 1 }
-					durationInTime = moment("1900-01-0" + EndOffSet + "T" + availTemp[2]).diff(("1900-01-01T" + availTemp[1]), 'seconds')
-
-					availTempObject =
-					{
-						"day": ObjectArray[i].day,
-						"payCodeEditTemplate": {
-							"systemGenerated": true,
-							"startDateTime": "1900-01-01T" + availTemp[1],
-							"endDateTime": "1900-01-0" + EndOffSet + "T" + availTemp[2],
-							"payCode": {
-								"qualifier": availTemp[0]
-							},
-							"durationInTime": durationInTime,
-						}
-					}
-					LoadedDataArray[y].items.push(availTempObject)
-				}
-
 			}
 		}
-	}
 
-	console.log(LoadedDataArray)
+		console.log(LoadedDataArray)
 
-	for (let y = 0, z = LoadingData.length; y < z; y++) {
-		for (let g = 0, h = LoadedDataArray.length; g < h; g++) {
-			console.log()
-			if (LoadingData[y][1] == LoadedDataArray[g].name) {
-				LoaderHOT.setDataAtCell(y, 0, "Pending")
-				LoaderHOT.setCellMeta(y, 0, 'className', 'YellowCellBackground')
+		for (let y = 0, z = LoadingData.length; y < z; y++) {
+			for (let g = 0, h = LoadedDataArray.length; g < h; g++) {
+				console.log()
+				if (LoadingData[y][1] == LoadedDataArray[g].name) {
+					LoaderHOT.setDataAtCell(y, 0, "Pending")
+					LoaderHOT.setCellMeta(y, 0, 'className', 'YellowCellBackground')
 
-				map = LoadedDataArray[g]
+					map = LoadedDataArray[g]
 
-				console.log(AccessToken)
-				var options4 = {
-					'method': 'PUT',
-					'url': 'https://' + SelectedConnection.url + "/api/v1/scheduling/schedule_pattern_templates?name=" + LoadedDataArray[g].name,
-					'headers': {
-						'appkey': SelectedConnection.appKey,
-						'Authorization': AccessToken,
-						'Content-Type': ['application/json']
-					},
-					body: JSON.stringify(map)
+					console.log(AccessToken)
+					var options4 = {
+						'method': 'PUT',
+						'url': 'https://' + SelectedConnection.url + "/api/v1/scheduling/schedule_pattern_templates?name=" + LoadedDataArray[g].name,
+						'headers': {
+							'appkey': SelectedConnection.appKey,
+							'Authorization': AccessToken,
+							'Content-Type': ['application/json']
+						},
+						body: JSON.stringify(map)
 
-				};
-				if (LoadingData[y][2] == "NOID"){options4.method = 'POST'}
+					};
+					if (LoadingData[y][2] == "NOID") { options4.method = 'POST' }
 
-				rp(options4)
-					.then(function (parsedBody) {
-						console.log(parsedBody)
-						console.log(parsedBody)
-						for (let y = 0, z = LoadingData.length; y < z; y++) {
-							RequestBody = JSON.parse(parsedBody)
-							if (LoadingData[y][1] == RequestBody.name) {
-								LoaderHOT.setDataAtCell(y, 0, "Successfully Imported")
-								LoaderHOT.setCellMeta(y, 0, 'className', 'GreenCellBackground')
-								LoaderHOT.render()
+					rp(options4)
+						.then(function (parsedBody) {
+							console.log(parsedBody)
+							console.log(parsedBody)
+							for (let y = 0, z = LoadingData.length; y < z; y++) {
+								RequestBody = JSON.parse(parsedBody)
+								if (LoadingData[y][1] == RequestBody.name) {
+									LoaderHOT.setDataAtCell(y, 0, "Successfully Imported")
+									LoaderHOT.setCellMeta(y, 0, 'className', 'GreenCellBackground')
+									LoaderHOT.render()
+								}
+
 							}
 
-						}
-
-					})
-					.catch(function (err) {
-						console.log('fail')
-						console.log(err)
-						for (let y = 0, z = LoadingData.length; y < z; y++) {
-							RequestBody = err.options.url.split("?name=")[1]
-							console.log(RequestBody, LoadingData[y][1])
-							if (LoadingData[y][1] == RequestBody) {
-								LoaderHOT.setDataAtCell(y, 0, JSON.parse(err.error).errorCode + JSON.parse(err.error).message)
-								LoaderHOT.setCellMeta(y, 0, 'className', 'RedCellBackground')
-								LoaderHOT.render()
+						})
+						.catch(function (err) {
+							console.log('fail')
+							console.log(err)
+							for (let y = 0, z = LoadingData.length; y < z; y++) {
+								RequestBody = err.options.url.split("?name=")[1]
+								console.log(RequestBody, LoadingData[y][1])
+								if (LoadingData[y][1] == RequestBody) {
+									LoaderHOT.setDataAtCell(y, 0, JSON.parse(err.error).errorCode + JSON.parse(err.error).message)
+									LoaderHOT.setCellMeta(y, 0, 'className', 'RedCellBackground')
+									LoaderHOT.render()
+								}
 							}
-						}
 
-					});
+						});
 
+				}
 			}
+
+		}
+	}
+	else {
+		LoadedDataArray = []
+		for (let y = 0, z = LoadingData.length; y < z; y++) {
+			LoadedData = ""
+			for (let i = 0, l = ObjectArray.length; i < l; i++) {
+				console.log(LoadingData[y][1], ObjectArray[i].personNumber + "--" + ObjectArray[i].name)
+				if (LoadingData[y][1] == ObjectArray[i].personNumber + "--" + ObjectArray[i].name) {
+					LoadedData =
+
+					{
+						"employeeSchedulePatterns": {
+							"do": {
+								"employeeSchedulePatternCreates": [
+									{
+										"override": true,
+										"employeeSchedulePattern": {
+											"employeeRef": {
+												"qualifier": ObjectArray[i].personNumber
+											},
+											"endDate": ObjectArray[i].EndDate,
+											"startDate": ObjectArray[i].StartDate,
+
+											"schedulePattern":
+											{
+												"name": ObjectArray[i].name,
+												"description": ObjectArray[i].description,
+												"timePeriodType": {
+													"name": ObjectArray[i].timePeriodType
+												},
+												"dayCount": ObjectArray[i].dayCount,
+												"startDate": ObjectArray[i].startDate,
+												"items": [],
+												"anchorStartDay": ObjectArray[i].anchorStartDay
+											}
+										}
+									}
+								]
+
+							}
+						}
+					}
+				}
+			}
+			if (LoadedData.employeeSchedulePatterns.do.employeeSchedulePatternCreates[0].employeeSchedulePattern.schedulePattern.anchorStartDay == null ||
+				LoadedData.employeeSchedulePatterns.do.employeeSchedulePatternCreates[0].employeeSchedulePattern.schedulePattern.anchorStartDay == "" ){
+					delete LoadedData.employeeSchedulePatterns.do.employeeSchedulePatternCreates[0].employeeSchedulePattern.schedulePattern.anchorStartDay}
+			LoadedDataArray.push(LoadedData)
+		}
+
+
+		for (let y = 0, z = LoadedDataArray.length; y < z; y++) {
+			for (let i = 0, l = ObjectArray.length; i < l; i++) {
+				if (LoadedDataArray[y].employeeSchedulePatterns.do.employeeSchedulePatternCreates[0].employeeSchedulePattern.schedulePattern.name == ObjectArray[i].name) {
+					console.log(ObjectArray[i])
+					if (ObjectArray[i].availabilities != null && ObjectArray[i].availabilities != "") {
+						availTempObject =
+						{
+							"day": ObjectArray[i].day,
+							"availabilityTemplate": {
+								"systemGenerated": true,
+								"segments": []
+							}
+						}
+
+						for (let zi = 0, zl = ObjectArray[i].availabilities.length; zi < zl; zi++) {
+
+							availTemp = ObjectArray[i].availabilities[zi].split('|')
+							availTempObject.availabilityTemplate.segments.push(
+								{
+									"availabilityType": {
+										"qualifier": availTemp[0]
+									},
+									"startTime": availTemp[1],
+									"endTime": availTemp[2]
+								}
+							)
+
+						}
+						LoadedDataArray[y].employeeSchedulePatterns.do.employeeSchedulePatternCreates[0].employeeSchedulePattern.schedulePattern.items.push(availTempObject)
+
+					}
+					else if (ObjectArray[i].shiftTemplates != null && ObjectArray[i].shiftTemplates != "") {
+
+						STTempObject =
+						{
+							"day": ObjectArray[i].day,
+							"shiftTemplate": {
+								"name": ObjectArray[i].shiftTemplates,
+							}
+						}
+						LoadedDataArray[y].employeeSchedulePatterns.do.employeeSchedulePatternCreates[0].employeeSchedulePattern.schedulePattern.items.push(STTempObject)
+					}
+
+
+
+					else if (ObjectArray[i].paycodes != null && ObjectArray[i].paycodes != "") {
+						availTemp = ObjectArray[i].paycodes.split('|')
+						if (availTemp[1] > availTemp[2]) { EndOffSet = 2 } else { EndOffSet = 1 }
+						durationInTime = moment("1900-01-0" + EndOffSet + "T" + availTemp[2]).diff(("1900-01-01T" + availTemp[1]), 'seconds')
+
+						availTempObject =
+						{
+							"day": ObjectArray[i].day,
+							"payCodeEditTemplate": {
+								"systemGenerated": true,
+								"startDateTime": "1900-01-01T" + availTemp[1],
+								"endDateTime": "1900-01-0" + EndOffSet + "T" + availTemp[2],
+								"payCode": {
+									"qualifier": availTemp[0]
+								},
+								"durationInTime": durationInTime,
+							}
+						}
+						LoadedDataArray[y].employeeSchedulePatterns.do.employeeSchedulePatternCreates[0].employeeSchedulePattern.schedulePattern.items.push(availTempObject)
+					}
+
+				}
+			}
+		}
+
+		console.log(LoadedDataArray)
+
+		for (let y = 0, z = LoadingData.length; y < z; y++) {
+			for (let g = 0, h = LoadedDataArray.length; g < h; g++) {
+				console.log()
+				if (LoadingData[y][1] == LoadedDataArray[g].employeeSchedulePatterns.do.employeeSchedulePatternCreates[0].employeeSchedulePattern.employeeRef.qualifier + '--' + LoadedDataArray[g].employeeSchedulePatterns.do.employeeSchedulePatternCreates[0].employeeSchedulePattern.schedulePattern.name) {
+					LoaderHOT.setDataAtCell(y, 0, "Pending")
+					LoaderHOT.setCellMeta(y, 0, 'className', 'YellowCellBackground')
+
+					map = LoadedDataArray[g]
+
+					console.log(AccessToken)
+					var options4 = {
+						'method': 'POST',
+						'url': 'https://' + SelectedConnection.url + "/api/v1/scheduling/employee_schedule_patterns/apply_create?partial_success=true",
+						'headers': {
+							'appkey': SelectedConnection.appKey,
+							'Authorization': AccessToken,
+							'Content-Type': ['application/json']
+						},
+						body: JSON.stringify(map)
+
+					};
+					if (LoadingData[y][2] == "NOID") { options4.method = 'POST' }
+
+					rp(options4)
+						.then(function (parsedBody) {
+							console.log(parsedBody)
+							console.log(parsedBody)
+							for (let y = 0, z = LoadingData.length; y < z; y++) {
+								RequestBody = JSON.parse(parsedBody)
+								if (LoadingData[y][1] == RequestBody.name) {
+									LoaderHOT.setDataAtCell(y, 0, "Successfully Imported")
+									LoaderHOT.setCellMeta(y, 0, 'className', 'GreenCellBackground')
+									LoaderHOT.render()
+								}
+
+							}
+
+						})
+						.catch(function (err) {
+							console.log('fail')
+							console.log(err)
+							for (let y = 0, z = LoadingData.length; y < z; y++) {
+								RequestBody = JSON.parse(err.options.body)
+								console.log(RequestBody, LoadingData[y][1])
+								if (LoadingData[y][1] == RequestBody.employeeSchedulePatterns.do.employeeSchedulePatternCreates[0].employeeSchedulePattern.employeeRef.qualifier + '--' + LoadedDataArray[g].employeeSchedulePatterns.do.employeeSchedulePatternCreates[0].employeeSchedulePattern.schedulePattern.name) {
+									LoaderHOT.setDataAtCell(y, 0, JSON.parse(err.error).errorCode + JSON.parse(err.error).message)
+									LoaderHOT.setCellMeta(y, 0, 'className', 'RedCellBackground')
+									LoaderHOT.render()
+								}
+							}
+
+						});
+
+				}
+			}
+
 		}
 
 	}
